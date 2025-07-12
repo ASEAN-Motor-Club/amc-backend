d83402171eaf7a657e8f438870689ac287b460b7
Freeman <4031216+fmnxl@users.noreply.github.com>
2025-07-12 18:26:13 +0700

    add more admin filtering

--- src/amc/admin.py
+++ src/amc/admin.py
@@ -10,21 +10,26 @@ from .models import (
   ServerLog
 )
 
+class CharacterInlineAdmin(admin.TabularInline):
+  model = Character
+  readonly_fields = ['name']
+
 @admin.register(Player)
 class PlayerAdmin(admin.ModelAdmin):
   list_display = ['unique_id']
-  search_fields = ['unique_id', 'character__name']
+  search_fields = ['unique_id', 'characters__name']
+  inlines = [CharacterInlineAdmin]
 
 class PlayerStatusLogInlineAdmin(admin.TabularInline):
   model = PlayerStatusLog
-  readonly_fields = ['character', 'original_log']
+  readonly_fields = ['character', 'timespan', 'duration']
   exclude = ['original_log']
 
 @admin.register(Character)
 class CharacterAdmin(admin.ModelAdmin):
   list_display = ['name', 'player__unique_id']
   list_select_related = ['player']
-  search_fields = ['player__unique_id']
+  search_fields = ['player__unique_id', 'name']
   inlines = [PlayerStatusLogInlineAdmin]
   readonly_fields = ['player']
 
@@ -37,14 +42,15 @@ class VehicleAdmin(admin.ModelAdmin):
 class CompanyAdmin(admin.ModelAdmin):
   list_display = ['name', 'owner', 'is_corp', 'first_seen_at']
   list_filter =  ['is_corp']
-  search_fields = ['owner__name', 'name']
   readonly_fields = ['owner']
+  search_fields = ['owner__name', 'owner__player__unique_id', 'name']
 
 @admin.register(PlayerChatLog)
 class PlayerChatLogAdmin(admin.ModelAdmin):
   list_display = ['timestamp', 'character', 'text']
   list_select_related = ['character', 'character__player']
   ordering = ['-timestamp']
+  search_fields = ['character__name', 'character__player__unique_id']
 
 @admin.register(PlayerStatusLog)
 class PlayerStatusLogAdmin(admin.ModelAdmin):
@@ -52,15 +58,18 @@ class PlayerStatusLogAdmin(admin.ModelAdmin):
   list_select_related = ['character', 'character__player']
   ordering = ['-timespan']
   readonly_fields = ['character', 'original_log']
+  search_fields = ['character__name', 'character__player__unique_id']
 
 @admin.register(PlayerVehicleLog)
 class PlayerVehicleLogAdmin(admin.ModelAdmin):
   list_display = ['timestamp', 'character', 'vehicle', 'action']
   list_select_related = ['character', 'character__player', 'vehicle']
   ordering = ['-timestamp']
+  search_fields = ['character__name', 'character__player__unique_id']
 
 @admin.register(ServerLog)
 class ServerLogAdmin(admin.ModelAdmin):
   list_display = ['timestamp', 'text', 'event_processed']
   ordering = ['-timestamp']
+  list_filter = ['event_processed']
 
