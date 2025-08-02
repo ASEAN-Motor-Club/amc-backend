65c1fe8b0b0c7c89fe8db6901d69b4cad78f4189
Freeman <4031216+fmnxl@users.noreply.github.com>
2025-08-02 23:22:16 +0700

    fix show_popup command permission

--- src/amc_cogs/moderation.py
+++ src/amc_cogs/moderation.py
@@ -7,7 +7,7 @@ class ModerationCog(commands.Cog):
     self.bot = bot
 
   @app_commands.command(name='send_popup', description='Sends a popup message to an in-game player')
-  @commands.has_permissions(administrator=True)
+  @app_commands.checks.has_permissions(administrator=True)
   async def send_popup(self, ctx, player_id: str, message: str):
     await show_popup(self.bot.http_client_mod, message, player_id=player_id)
     await ctx.response.send_message('Popup sent', ephemeral=True)
