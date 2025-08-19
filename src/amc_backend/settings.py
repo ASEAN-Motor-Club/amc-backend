f4b14a10f4aba9cc87255d825b57cfbf10b6e7da
Freeman <4031216+fmnxl@users.noreply.github.com>
2025-08-19 13:01:53 +0700

    enable console logging

--- src/amc_backend/settings.py
+++ src/amc_backend/settings.py
@@ -28,6 +28,20 @@ DEBUG = bool(os.environ.get('DEBUG'))
 
 ALLOWED_HOSTS = [*os.environ.get('ALLOWED_HOSTS', '').split(' '), 'localhost', '127.0.0.1']
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "WARNING",
+    },
+}
+
 
 # Application definition
 
