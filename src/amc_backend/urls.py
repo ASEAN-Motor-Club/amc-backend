6f77980b96ae54b1b3d23fd17e081fbdd8c7601c
Freeman <4031216+fmnxl@users.noreply.github.com>
2025-09-28 11:50:19 +0700

    serve statics on debug

--- src/amc_backend/urls.py
+++ src/amc_backend/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from .api import api
 from amc.views import login_with_token
 
@@ -25,3 +27,7 @@ urlpatterns = [
     path('api/login/token/', login_with_token, name='token_login_api'),
     path('api/', api.urls),
 ]
+
+if settings.DEBUG:
+  urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
