443dec674a20ad409c08f7917dd2c2ea77236734
Freeman <4031216+fmnxl@users.noreply.github.com>
2025-12-31 16:03:00 +0700

    refactor: leave /rp handling to mod

--- src/amc/commands/rp_rescue.py
+++ src/amc/commands/rp_rescue.py
@@ -3,12 +3,10 @@ from amc.command_framework import registry, CommandContext
 from amc.models import RescueRequest
 from django.contrib.gis.geos import Point
 from amc.mod_server import (
-    get_rp_mode, despawn_player_vehicle,
-    toggle_rp_session, despawn_by_tag, set_character_name,
+    get_rp_mode, set_character_name,
     get_players as get_players_mod, list_player_vehicles,
     send_system_message
 )
-from amc.utils import with_verification_code
 from amc.vehicles import format_key_string
 from django.utils import timezone
 from datetime import timedelta
@@ -18,23 +16,7 @@ from amc.mod_server import get_player
 
 @registry.register(["/rp_mode", "/rp"], description=gettext_lazy("Toggle Roleplay Mode"), category="RP & Rescue") # type: ignore
 async def cmd_rp_mode(ctx: CommandContext, verification_code: str = ""):
-    is_rp_mode = await get_rp_mode(ctx.http_client_mod, ctx.character.guid)
-    
     if verification_code:
-        # Verify and Toggle
-        code_gen, code_verified = with_verification_code((ctx.character.guid, is_rp_mode), verification_code)
-        if not code_verified:
-            await ctx.reply(_("<Title>Code Incorrect</>\nTry: <Highlight>/rp_mode {code_gen}</>").format(code_gen=code_gen.upper()))
-            return
-        
-        await despawn_player_vehicle(ctx.http_client_mod, ctx.player.unique_id)
-        try:
-            await toggle_rp_session(ctx.http_client_mod, ctx.character.guid)
-        except Exception:
-            pass
-        await despawn_by_tag(ctx.http_client_mod, f'rental-{ctx.character.guid}')
-        
-        # Refresh State
         is_rp_mode = await get_rp_mode(ctx.http_client_mod, ctx.character.guid)
         ctx.character.rp_mode = is_rp_mode
         await ctx.character.asave(update_fields=['rp_mode'])
@@ -48,18 +30,7 @@ async def cmd_rp_mode(ctx: CommandContext, verification_code: str = ""):
         if new_name:
             await set_character_name(ctx.http_client_mod, ctx.character.guid, new_name)
         
-        await asyncio.sleep(1)
-        msg = _("<Title>Roleplay Mode Enabled</>\n<EffectGood>Enabled!</>") if is_rp_mode else _("<Title>Roleplay Mode Disabled</>")
-        await ctx.reply(msg)
 
-    else:
-        # Request Confirmation
-        code_gen, _ignored = with_verification_code((ctx.character.guid, is_rp_mode), "")
-        status = '<EffectGood>ON</>' if is_rp_mode else '<Warning>OFF</>'
-        notes = _("To turn off, resend with code:") if is_rp_mode else _("Enabling RP mode gives bonuses but risks cargo/vehicle loss on recovery.\n<Warning>All vehicles will despawn on toggle!</>")
-        await ctx.reply(_("<Title>Roleplay Mode</>\nStatus: {status}\n\n{notes}\n<Highlight>/rp_mode {code_gen}</>").format(
-            status=status, notes=notes, code_gen=code_gen.upper()
-        ))
 
 @registry.register("/rescue", description=gettext_lazy("Calls for rescue service"), category="RP & Rescue") # type: ignore
 async def cmd_rescue(ctx: CommandContext, message: str = ""):
